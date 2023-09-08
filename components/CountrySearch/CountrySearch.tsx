195fa1f05325545a0d24331f03d6e140e487dee7
ozanbilgic <ozan.bilgic@nextedy.com>
2023-09-08 22:40:51 +0200

    Add Autocomplete component

--- components/CountrySearch/CountrySearch.tsx
+++ components/CountrySearch/CountrySearch.tsx
@@ -1,26 +1,24 @@
-import { IconButton, InputBase, Paper } from "@mui/material";
+import { Autocomplete, TextField } from "@mui/material";
 import { useEffect, useState } from "react";
 import { useGlobalContext } from "../../layouts/LayoutDefault/context";
-import { CountryCard } from "../CountryCard/CountryCard";
 import { CountryDialog } from "../CountryDialog/CountryDialog";
-import SearchIcon from "@mui/icons-material/Search";
 import { ICountryData } from "../../layouts/LayoutDefault/LayoutDefault.types";
-import { countryDialog__Paper } from "./CountrySearch.styles";
+import { IConfiguredData } from "./CountrySearch.types";
 
 export function CountrySearch() {
   const { isSearchDialogOpen, setIsSearchDialogOpen, currentUser } =
     useGlobalContext();
-  const [searchValue, setSearchValue] = useState("");
-  const [isDataLoading, setIsDataLoading] = useState(false);
-  const [allCountries, setAllCountries] = useState<ICountryData[]>();
+  const [allCountries, setAllCountries] = useState<IConfiguredData[]>();
   const getAllCountries = async () => {
-    setIsDataLoading(true);
     const res = await fetch("https://restcountries.com/v3.1/all");
     const data: ICountryData[] = await res.json();
     data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+    const configuredData = data.map((item) => ({
+      label: item.name.common,
+      link: item.cca3,
+    }));
 
-    setAllCountries(data);
-    setIsDataLoading(false);
+    setAllCountries(configuredData);
   };
 
   useEffect(() => {
@@ -38,31 +36,18 @@ export function CountrySearch() {
     <CountryDialog
       open={isSearchDialogOpen}
       handleClose={handleClose}
-      header={
-        <Paper component="form" sx={countryDialog__Paper}>
-          <IconButton sx={{ p: "10px" }} aria-label="menu" disabled={true}>
-            <SearchIcon />
-          </IconButton>
-          <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="Search"
-            inputProps={{ "aria-label": "Search" }}
-            value={searchValue}
-            onChange={(a) =>
-              setSearchValue((a.target as HTMLInputElement).value)
-            }
-            autoFocus={true}
-          />
-        </Paper>
-      }
+      header={null}
       content={
-        <CountryCard
-          data={allCountries?.filter((country) => {
-            return country.name.common.toLowerCase() === ""
-              ? country.name.common
-              : country.name.common.toLowerCase().includes(searchValue);
-          })}
-          isDataLoading={isDataLoading}
+        <Autocomplete
+          disablePortal
+          id="combo-box"
+          options={allCountries}
+          sx={{ width: "auto", height: "auto" }}
+          onChange={(event: any, option: any) => {
+            window.location.href = option.link;
+          }}
+          renderInput={(params) => <TextField {...params} label="Search" />}
+          fullWidth
         />
       }
       maxWidth={"xl"}
